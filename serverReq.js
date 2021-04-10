3881debdb0cc6c106c69ad79372dce89c6dca8cc
Elias Dagher <eliasdagher77@hotmail.com>
2021-04-09 23:57:11 -0600

    Second commit

--- serverReq.js
+++ serverReq.js
@@ -6,7 +6,11 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true } );
 mongoose.set('useCreateIndex', true);
 
 var MovCategory = new Schema({
-    Title: {type:String, required:true}, Year: {type:Date, required:true}, Genre: {type:String, required:true, enum:["Action", "Comedy", "Drama", "Horror", "Thriller", "Sci-fi"]}, Actors: {type:[{ActorName:String, CharacterName:String}], required:true, ImageURI: {type:String, required: true}}});
+    Title: {type:String, required:true},
+    Year: {type:Date, required:true},
+    Genre: {type:String, required:true, enum:["Action", "Comedy", "Drama", "Horror", "Thriller", "Sci-fi"]},
+    Actors: {type:[{ActorName:String, CharacterName:String}], required:true},
+        ImageURI: {type:String, required: false}});
 
 
 var Movie = mongoose.model('Movie', MovCategory);
